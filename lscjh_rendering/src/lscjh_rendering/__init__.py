cd11c4b9e970bf1422ed3b110be5e5e8d3844727
Joshua Hegedus <josh.hegedus@outlook.com>
2026-04-04 11:41:43 +0200

    moved out the renderer into its own repo

--- lscjh_rendering/src/lscjh_rendering/__init__.py
+++
@@ -1,9 +0,0 @@
-# SPDX-FileCopyrightText: 2026-present Joshua Hegedus <josh.hegedus@outlook.com>
-#
-# SPDX-License-Identifier: MIT
-
-from .tileset import Tileset
-from .renderer import Renderer
-from .map import Map
-
-__all__ = ["Tileset", "Renderer", "Map"]
--- lscjh_rendering/src/lscjh_rendering/lib.py
+++
@@ -1,4 +0,0 @@
-from typing import Callable
-
-Layer = list[list[tuple[int, int]]]
-DrawCallback = Callable[[int, int, int, int], None]
